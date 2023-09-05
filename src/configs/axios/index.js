977f9a9bab0b87e57a3a4ec49aff5ea8f37f3173
Rafidimas17 <rafidimas1706@gmail.com>
2023-09-06 06:03:18 +0700

    update json, edit CI command and delete react toastify

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -6,10 +6,6 @@ const instance = axios.create({
   baseURL: `${process.env.REACT_APP_HOST}/api-v1/`,
 });
 
-// const instance = axios.create({
-//   baseURL: `${process.env.REACT_APP_HOST}/users/`,
-// });
-
 instance.interceptors.response.use(
   (response) => response,
   errorResponseHandler
