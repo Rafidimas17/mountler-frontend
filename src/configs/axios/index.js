791992575379d65f21393bbf6c1ddb7f55196afb
Rafidimas17 <rafidimas1706@gmail.com>
2023-09-06 06:08:36 +0700

    edit axios code without errorHandler from react-toastify

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -1,14 +1,31 @@
 import axios from "axios";
 
-import errorResponseHandler from "./errorResponseHandler";
-
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_HOST}/api-v1/`,
 });
 
 instance.interceptors.response.use(
   (response) => response,
-  errorResponseHandler
+  (error) => {
+    if (error.response) {
+      // Handle HTTP error responses (e.g., status code is not 2xx)
+      console.error(
+        "HTTP error:",
+        error.response.status,
+        error.response.statusText
+      );
+      // You can add more specific error handling here
+    } else if (error.request) {
+      // Handle network errors (e.g., request not sent)
+      console.error("Network error:", error.request);
+      // You can add more specific error handling here
+    } else {
+      // Handle other errors
+      console.error("Error:", error.message);
+    }
+    // Throw the error to be caught in the calling code
+    throw error;
+  }
 );
 
 export default instance;
